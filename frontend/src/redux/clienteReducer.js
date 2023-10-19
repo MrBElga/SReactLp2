dcd22656c4b6237d36a2c7975de4d840aff41124
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2023-10-19 10:51:20 -0300

    Update redux

---
+++ frontend/src/redux/clienteReducer.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+import ESTADO from "../recursos/estado";
+
+const clienteSlice = createSlice({
+    name:'cliente',
+    initialState:{
+        status: ESTADO.OCIOSO,
+        mensagem:'',
+        listaClientes:[]
+    },
+    reducers:{
+        
+        adicionar:(state, action)=>{
+            state.listaClientes.push(action.payload);
+        },
+        remover:(state,action)=>{
+            state.listaClientes = state.listaClientes.filter(cliente => cliente.cpf !== action.payload.cpf);
+        },
+        atualizar:(state,action)=>{
+            const listaTemporariaClientes = state.listaClientes.filter(cliente => cliente.cpf !== action.payload.cpf);
+            state.listaClientes = [...listaTemporariaClientes, action.payload.cliente];
+        }
+
+    }
+});
+export const {adicionar,remover,atualizar} = clienteSlice.actions; 
+export default clienteSlice.reducer;
