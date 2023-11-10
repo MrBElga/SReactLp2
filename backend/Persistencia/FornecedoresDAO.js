0c449eafdd76745a31a2a741a7f252ff6b53645d
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2023-11-10 08:55:50 -0300

    Update DAO

--- backend/Persistencia/FornecedoresDAO.js
+++ backend/Persistencia/FornecedoresDAO.js
@@ -1,55 +1,61 @@
+import Fornecedor from '../Modelo/Fornecedor.js'
 export default class FornecedorDAO {
   constructor() {}
 
   async gravar(fornecedor, conexao) {
-    const sql =
-      "INSERT INTO fornecedores (forn_cnpj, forn_nome, forn_telefone, forn_celular, forn_endereco, forn_numero, forn_bairro, forn_cidade, forn_uf, forn_cep, forn_email,usu_prior) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    const valores = [
-      fornecedor.cnpj,
-      fornecedor.nome,
-      fornecedor.telefone,
-      fornecedor.celular,
-      fornecedor.endereco,
-      fornecedor.numero,
-      fornecedor.bairro,
-      fornecedor.cidade,
-      fornecedor.uf,
-      fornecedor.cep,
-      fornecedor.email,
-      fornecedor.prior,
-    ];
+    if(fornecedor instanceof Fornecedor )
+    {
+      const sql = "INSERT INTO fornecedores (forn_cnpj, forn_nome, forn_telefone, forn_celular, forn_endereco, forn_numero, forn_bairro, forn_cidade, forn_uf, forn_cep, forn_email,usu_prior) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+      const valores = [
+        fornecedor.cnpj,
+        fornecedor.nome,
+        fornecedor.telefone,
+        fornecedor.celular,
+        fornecedor.endereco,
+        fornecedor.numero,
+        fornecedor.bairro,
+        fornecedor.cidade,
+        fornecedor.uf,
+        fornecedor.cep,
+        fornecedor.email,
+        fornecedor.prior,
+      ];
 
-    try {
-      const [result] = await conexao.execute(sql, valores);
-      return result.insertId;
-    } catch (error) {
-      throw error;
+      try {
+        const [result] = await conexao.execute(sql, valores);
+        return result.insertId;
+      } catch (error) {
+        throw error;
+      }
     }
   }
 
   async atualizar(fornecedor, conexao) {
-    const sql =
-      "UPDATE fornecedores SET forn_cnpj = ?, forn_nome = ?, forn_telefone = ?, forn_celular = ?, forn_endereco = ?, forn_numero = ?, forn_bairro = ?, forn_cidade = ?, forn_uf = ?, forn_cep = ?, forn_email = ?, usu_prior = ? WHERE codigo = ?";
-    const valores = [
-      fornecedor.cnpj,
-      fornecedor.nome,
-      fornecedor.telefone,
-      fornecedor.celular,
-      fornecedor.endereco,
-      fornecedor.numero,
-      fornecedor.bairro,
-      fornecedor.cidade,
-      fornecedor.uf,
-      fornecedor.cep,
-      fornecedor.email,
-      fornecedor.codigo,
-      fornecedor.prior,
-    ];
-    console.log(valores);
-    try {
-      await conexao.execute(sql, valores);
-    } catch (error) {
-      throw error;
+    if(fornecedor instanceof Fornecedor )
+    {
+      const sql =
+        "UPDATE fornecedores SET forn_cnpj = ?, forn_nome = ?, forn_telefone = ?, forn_celular = ?, forn_endereco = ?, forn_numero = ?, forn_bairro = ?, forn_cidade = ?, forn_uf = ?, forn_cep = ?, forn_email = ?, usu_prior = ? WHERE codigo = ?";
+      const valores = [
+        fornecedor.cnpj,
+        fornecedor.nome,
+        fornecedor.telefone,
+        fornecedor.celular,
+        fornecedor.endereco,
+        fornecedor.numero,
+        fornecedor.bairro,
+        fornecedor.cidade,
+        fornecedor.uf,
+        fornecedor.cep,
+        fornecedor.email,
+        fornecedor.codigo,
+        fornecedor.prior,
+      ];
+      console.log(valores);
+      try {
+        await conexao.execute(sql, valores);
+      } catch (error) {
+        throw error;
+      }
     }
   }
 
@@ -76,12 +82,15 @@ export default class FornecedorDAO {
   }
 
   async excluir(fornecedor, conexao) {
-    const sql = "DELETE FROM fornecedores WHERE codigo = ?";
+    if(fornecedor instanceof Fornecedor )
+    {
+      const sql = "DELETE FROM fornecedores WHERE codigo = ?";
 
-    try {
-      await conexao.execute(sql, [fornecedor.codigo]);
-    } catch (error) {
-      throw error;
+      try {
+        await conexao.execute(sql, [fornecedor.codigo]);
+      } catch (error) {
+        throw error;
+      }
     }
   }
 }
