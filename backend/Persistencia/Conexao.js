4dfbf2b3252920dd9c8b9f10af6003cde7cb87b9
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2023-11-13 10:16:07 -0300

    Update conexao

--- backend/Persistencia/Conexao.js
+++ backend/Persistencia/Conexao.js
@@ -13,8 +13,6 @@ function conectar() {
     password: "",
     waitForConnections: true,
     connectionLimit: 10,
-    maxIdle: 10,
-    idleTimeout: 60000,
     queueLimit: 0,
     enableKeepAlive: true,
     keepAliveInitialDelay: 0,
